e47494794df67f58fec26c365ed8c785653c7b6b
Musa Alp <musa.alp@justeattakeaway.com>
2022-09-13 01:28:21 +0200

    feat: Add user as a domain entity

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    internal class User
+    {
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public static User CreateNew(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+
+            return new User(name);
+        }
+    }
+}
